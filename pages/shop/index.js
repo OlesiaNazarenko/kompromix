52d71c78f18de4b4fee7f169c1611cae827e54fa
Olesia Nazarenko <80903117+OlesiaNazarenko@users.noreply.github.com>
2022-02-23 22:15:17 +0200

    modal added

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,5 +1,6 @@
 import Header from "../../components/header/Header";
 import Head from "next/head";
+import BaseModal from "../../components/modal/BaseModal";
 export default function Shop() {
   return (
     <>
@@ -8,6 +9,7 @@ export default function Shop() {
       </Head>
       <Header />
       <h1>Shop</h1>
+      <BaseModal className="buttonGetTheCall" buttonName="Shop" />
     </>
   );
 }
