08afb5f507a0a6e854597456a01a3c6ad98cbeba
Olesia Nazarenko <80903117+OlesiaNazarenko@users.noreply.github.com>
2022-03-15 21:07:31 +0200

    ourBenefits section done

--- components/homePage/HomePage.jsx
+++ components/homePage/HomePage.jsx
@@ -1,20 +1,18 @@
 import s from "./HomePage.module.css";
-import PeopleIcon from "@mui/icons-material/People";
-import AvTimerIcon from "@mui/icons-material/AvTimer";
-import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
-import Image from "next/image";
-import RoofingElements from "../../public/details.jpg";
-
-import KeyRoof from "../../public/keyRoof.jpg";
+import data from "../../json/ourBenefits.json";
+import OurBenefits from "../ourBenefits/OurBenefits";
 import classNames from "classnames";
 import ServicesSlider from "../servicesSlider/ServicesSlider";
-import BaseModal from "../modal/BaseModal";
 export default function HomePage() {
+  const { slides } = data;
   return (
     <>
       <section className={s.homePageWrap}>
         <div className="container">
           <ServicesSlider />
+        </div>
+        <div className={classNames(s.ourBenefits, "ourBenefits")}>
+          <OurBenefits slides={slides} />
         </div>
       </section>
     </>
