37632f48d9126e62863522ac00bf62c641dbf3eb
Olesia Nazarenko <80903117+OlesiaNazarenko@users.noreply.github.com>
2022-03-31 18:55:52 +0300

    fix bugs

--- db/db.js
+++ db/db.js
@@ -1,18 +1,15 @@
-import { FirebaseApp, initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-import "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_DB_API_KEY,
-  authDomain: process.env.FIREBASE_DB_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_DB_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_DB_STORAGE_BASKET,
-  messagingSenderId: process.env.FIREBASE_DB_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_DB_APP_ID,
-  measurementId: process.env.FIREBASE_DB_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_DB_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DB_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_DB_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_DB_STORAGE_BASKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_DB_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_DB_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_DB_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+console.log(db);
 export default db;
